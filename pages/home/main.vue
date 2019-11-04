4750dd4d04ea935c961d0686918adef881b1f904
jsaaron <401545553@qq.com>
2019-11-04 15:37:37 +0800

    123

--- pages/home/main.vue
+++ pages/home/main.vue
@@ -10,7 +10,18 @@
 		</view>
 
 		<view class="qiun-columns">
-			<view class="qiun-charts"><canvas canvas-id="canvasPie" id="canvasPie" class="charts"></canvas></view>
+			<view class="qiun-charts"><canvas canvas-id="canvasRing" id="canvasRing" class="charts"></canvas></view>
+		</view>
+
+		<view class="qiun-bottom">
+			<view class="qiun-names">
+							<block v-for="(item, index) in serieNames" :key="index">
+				<view class="qiun-row">
+					<text class="qiun-dot"></text>
+					<text>{{item.name}}:{{item.data}}%</text>
+				</view>
+				</block>
+			</view>
 		</view>
 	</view>
 </template>
@@ -21,7 +32,6 @@ import mHeader from './header.vue';
 import uCharts from '@/components/u-charts/u-charts.js';
 var _self;
 var canvaRing = null;
-var canvaPie = null;
 
 export default {
 	data() {
@@ -29,7 +39,8 @@ export default {
 			cWidth: '',
 			cHeight: '',
 			pixelRatio: 1,
-			serverData: ''
+			serverData: '',
+			serieNames: []
 		};
 	},
 	onLoad() {
@@ -47,33 +58,44 @@ export default {
 		...mapActions('home', ['getHomeData']),
 
 		getServerData() {
-			let chartData = {
-				series: []
-			};
-			for (var i = 0; i < 20; i++) {
-				chartData.series.push({
-					name: '业务员' + i,
-					data: 5
-				});
-			}
-			_self.showRing('canvasPie', chartData);
+			uni.request({
+				url: 'https://www.ucharts.cn/data.json',
+				data: {},
+				success: function(res) {
+					console.log(res.data.data);
+					let Ring = { series: [] };
+					//这里我后台返回的是数组，所以用等于，如果您后台返回的是单条数据，需要push进去
+					Ring.series = res.data.data.Ring.series;
+					//自定义文案示例，需设置format字段
+					for (let i = 0; i < Ring.series.length; i++) {
+						Ring.series[i].format = () => {
+							return Ring.series[i].name + Ring.series[i].data;
+						};
+					}
+					_self.textarea = JSON.stringify(res.data.data.Ring);
+					_self.serieNames = Ring.series;
+					_self.showRing('canvasRing', Ring);
+				},
+				fail: () => {
+					_self.tips = '网络错误，小程序端请检查合法域名';
+				}
+			});
 		},
 
-		//===================================
-
 		showRing(canvasId, chartData) {
-			canvaPie = new uCharts({
+			canvaRing = new uCharts({
 				$this: _self,
 				canvasId: canvasId,
 				type: 'ring',
 				fontSize: 11,
+				padding: [20, 5, 5, 5],
 				legend: {
 					show: false
 				},
 				background: '#FFFFFF',
 				pixelRatio: _self.pixelRatio,
 				series: chartData.series,
-				animation: true,
+				animation: false,
 				width: _self.cWidth * _self.pixelRatio,
 				height: _self.cHeight * _self.pixelRatio,
 				disablePieStroke: true,
@@ -97,6 +119,21 @@ export default {
 				}
 			});
 		}
+
+		// getServerData() {
+		// 	let chartData = {
+		// 		series: []
+		// 	};
+		// 	for (var i = 0; i < 20; i++) {
+		// 		chartData.series.push({
+		// 			name: '业务员' + i,
+		// 			data: 5
+		// 		});
+		// 	}
+		// 	_self.showRing('canvasPie', chartData);
+		// },
+
+		//===================================
 	}
 };
 </script>
@@ -123,7 +160,6 @@ export default {
 		background: rgb(96, 168, 252);
 	}
 }
-
 .qiun-padding {
 	padding: 2%;
 	width: 96%;
@@ -159,12 +195,35 @@ export default {
 }
 .qiun-charts {
 	width: 750upx;
-	height: 500rpx;
+	height: 500upx;
 	background-color: #ffffff;
 }
 .charts {
 	width: 750upx;
-	height: 500rpx;
+	height: 500upx;
 	background-color: #ffffff;
+}
+
+.qiun-bottom {
+	padding: 30rpx 20rpx;
+	font-size: 25px;
+}
+
+.qiun-names{
+	@include flex-h-left;
+	flex-wrap: wrap;
+}
+.qiun-row {
+	@include flex-h;
+	width: 25%;
+	padding:10rpx 0;
+}
+.qiun-dot {
+	width: 20rpx;
+	height: 20rpx;
+	border-radius: 50%;
+	background: red;
+	background: #0069d9;
+	margin-right: 5rpx;
 }
 </style>
